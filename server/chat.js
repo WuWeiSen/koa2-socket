ad7f84c079ec5adec4f28ea71b87af2b91ece57c
kraaas <570453516@qq.com>
2016-11-22 17:58:45 +0800

    update

--- server/chat.js
+++ server/chat.js
@@ -17,18 +17,44 @@ export default class Chat {
         })
       })
 
-      // 2.新方案
-      socket.on('newSchemeEvent', function(data) {
-        socket.broadcast.emit('newSchemeEvent', {
+       // 1.新出行计划
+      socket.on('cancelPlanEvent', function(data) {
+        socket.broadcast.emit('cancelPlanEvent', {
           planId: 1,
           planNum: 'asdfsdf'
         })
       })
 
+      // 2.新方案
+      socket.on('newSchemeEvent', function(data) {
+        socket.broadcast.emit('newSchemeEvent', {
+          planId: 1,
+          planNum: 'asdfsdf',
+          schemeId: 1,
+          schemeNum: 2
+        })
+      })
+
       // 3.方案聊天
-      socket.on('schemeChatEvent', function(data) {
-        socket.broadcast.emit('schemeChatEvent', {
-          logo: 1,
+      socket.on('chatEvent', function(data) {
+        socket.broadcast.emit('chatEvent', {
+          userId: 1, //消息发送人ID
+          userName: 'liang', //发送人名称
+          logo: null, //图标
+          sendTime: Date.now(), //发送时间
+          message: 'msg from tmc', //消息内容
+          messageId: 1, //消息ID
+          bussinessId: 896, //业务ID
+          bussinessType: 1, //业务关联类型
+          companyName: '腾邦国际', //公司名称
+          bussinessNum: 'C001' //业务关联编号
+        })
+      })
+
+      // 4.出行计划聊天
+      socket.on('planChatEvent', function(data) {
+        socket.broadcast.emit('planChatEvent', {
+          logo: 826,
           userName: 'asdfsdf',
           isTc: true,
           sendTime: 1478076873762,
@@ -36,30 +62,36 @@ export default class Chat {
         })
       })
 
-      // 4.出行计划聊天
-      socket.on('planChatEvent', function(data) {
-
-      })
-
       // 5.满意支付方案
       socket.on('satisfactionSchemeEvent', function(data) {
         socket.broadcast.emit('satisfactionSchemeEvent', {
-          logo: 1,
-          userName: 'asdfsdf',
-          isTc: true,
-          sendTime: 1478076873762,
-          message: 'new msg'
+          planId: 1,
+          planNum: 'C00001482',
+          schemeId: '1',
+          schemeNum: 'C00001482',
+          createTime: Date.now()
+        })
+      })
+
+      // 5.确认订单
+      socket.on('confirmOrderEvent', function(data) {
+        socket.broadcast.emit('confirmOrderEvent', {
+          planId: 1,
+          planNum: 'C00001482',
+          orderId: '1',
+          orderNum: 'C00001482',
+          createTime: Date.now()
         })
       })
 
       // 6.不满意方案
       socket.on('unsatisfactionSchemeEvent', function(data) {
         socket.broadcast.emit('unsatisfactionSchemeEvent', {
-          logo: 1,
-          userName: 'asdfsdf',
-          isTc: true,
-          sendTime: 1478076873762,
-          message: 'new msg'
+          planId: 1,
+          planNum: 'C00001482',
+          schemeId: '1',
+          schemeNum: 'C00001482',
+          createTime: Date.now()
         })
       })
 
